911620eff7e34f52b5d1ca5378c27736165be2e4
SKNabiev <SKNabiev@KB37-309-MMP>
2024-09-10 10:49:43 +0300

    Добавьте файлы проекта.

---
+++ Urok1/Source.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main() 
+{
+	int int = 20;
+	str s1 = "hello";
+	char ch1 = 'sd';
+	double d1 = 25;
+	float f1 = 25,6;
+	int a1, b1, c1;
+	char int = 'A';
+	int a1 = 25;
+	char a1 = 'H';
+	string str = 'Hello';
+	char b1 = 'B'
+	int a2, a3, int c3 = 18;
+	s1 = 'hi';
+	char[] = "lets go";
+
+	return 0;
+}
