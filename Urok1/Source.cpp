6c474493d756c2364d70ecb50ada245af7ba4b4d
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2024-09-11 08:57:28 +0300

    Source.cpp

--- Urok1/Source.cpp
+++ Urok1/Source.cpp
@@ -1,22 +1,36 @@
 #include<iostream>
 #include<string>
 using namespace std;
-int main() 
-{
-	int int = 20;
-	str s1 = "hello";
-	char ch1 = 'sd';
-	double d1 = 25;
-	float f1 = 25,6;
-	int a1, b1, c1;
-	char int = 'A';
-	int a1 = 25;
-	char a1 = 'H';
-	string str = 'Hello';
-	char b1 = 'B'
-	int a2, a3, int c3 = 18;
-	s1 = 'hi';
-	char[] = "lets go";
 
+int integ = 25;
+int teg;
+
+int main()
+{
+	//cout << "sdsfsf";
+	//wchar_t i1{ L'i' };
+	//std::wcout << i1 << '\n';
+	
+	//c12 = 25;
+	//char w[] = "west";
+	//teg = 55;
+	//string w1[] = "Lady";
+	//int int = 20;
+	//str s1 = "hello";
+	//char ch1 = 'sd';
+	//double d1 = 25;
+	//float f1 = 25,6;
+	//int a1, b1, c1;
+	//char int = 'A';
+	//int a1 = 25;
+	//char a1 = 'H';
+	//string str = 'Hello';
+	//char b1 = 'B'
+	//int a2, a3, int c3 = 18;
+	//s1 = 'hi';
+	//char[] = "lets go";
+	//{ int inerg = 22; }
+	//cout << integ << endl<< inerg <<endl ;
 	return 0;
 }
+//teg = 85;
